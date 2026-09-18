1f02b0391cf854287c37f82df243510230482df8
Adil <220942641+Adil-4fn@users.noreply.github.com>
2026-09-18 11:13:33 +0530

    add notification timeout and change to original directory

--- journal.sh
+++ journal.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-
-# idea is to use 'touch' to open the current day journal
-# get the date, format the file name and path, use touch to make it
-# and then open it in nvim
-# next stage would be terminal version vs niri version
-
-NOTES_DIRECTORY="/home/adil/notes/tempjourn/"
+NOTES_DIRECTORY="/home/adil/notes/journal/"
 #directory check
 [ -d "$NOTES_DIRECTORY" ] ||{
     echo "Directory does not exist"
@@ -16,10 +10,11 @@ NOTES_DIRECTORY="/home/adil/notes/tempjourn/"
 today=$(date +%Y-%m-%d)
 daily_note="${NOTES_DIRECTORY}/${today}.md"
 
-touch "$daily_note" ||{
-    notify-send 'journal' "failed to make note"
+touch "$daily_note" ||
+{
+    notify-send --expire-time=3000 'journal' "failed to make note"
     exit 1
 }
 
-#nvim "$daily_note"
+nvim "$daily_note"
 
